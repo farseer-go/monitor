55eff90e03c1ef033554f02c34817c62be719bc7
ffkcoding <Jams568952@outlook.com>
2024-10-09 18:31:33 +0800

    monitor 调整

--- addMonitor.go
+++ addMonitor.go
@@ -1,10 +1,8 @@
 package monitor
 
 import (
-	"context"
 	"github.com/farseer-go/collections"
 	"github.com/farseer-go/fs/core"
-	"github.com/farseer-go/fs/exception"
 	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/fs/parse"
 	"github.com/farseer-go/utils/ws"
@@ -26,37 +24,23 @@ type monitorFunc func() collections.Dictionary[string, any]
 
 // AddMonitor 添加一个监控，运行前先休眠
 // interval:任务运行的间隔时间
-func AddMonitor(interval time.Duration, monitorFn monitorFunc, ctx context.Context) {
+func AddMonitor(interval time.Duration, monitorFn monitorFunc) {
 	// 不立即运行，则先休眠interval时间
 	if interval <= 0 {
 		panic("interval参数，必须大于0")
 	}
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-time.After(interval):
-				dic := monitorFn()
-				wsConnectSendMsg(dic)
-			}
-		}
-	}()
-}
-
-// wsConnectSendMsg 发送消息
-func wsConnectSendMsg(dic collections.Dictionary[string, any]) {
-	var err error
-	if wsClient == nil || wsClient.IsClose() {
+	for {
+		var err error
 		address := defaultServer.getAddress()
 		wsClient, err = ws.Connect(address, 8192)
 		if err != nil {
 			flog.Warningf("[%s]wsmonitor连接fops失败：%s", core.AppName, err.Error())
+			time.Sleep(3 * time.Second)
+			continue
 		}
-	}
-	// 发送消息
-	exception.Try(func() {
-		if wsClient != nil && !wsClient.IsClose() {
+		for {
+			dic := monitorFn()
+			// 发送消息
 			err = wsClient.Send(SendContentVO{
 				AppId:   parse.ToString(core.AppId),
 				AppName: core.AppName,
@@ -64,11 +48,11 @@ func wsConnectSendMsg(dic collections.Dictionary[string, any]) {
 			})
 			if err != nil {
 				flog.Warningf("[%s]监控发送消息失败：%s", core.AppName, err.Error())
+				break
 			}
+			time.Sleep(interval)
 		}
-	}).CatchException(func(exp any) {
-		if exp != nil {
-			flog.Warningf("[%s]监控发送消息异常：%s", core.AppName, exp)
-		}
-	})
+		// 断开后重连
+		time.Sleep(3 * time.Second)
+	}
 }
